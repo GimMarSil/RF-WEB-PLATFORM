6887721db93b4290a5c1b2cf27e17bdf81e0f04d
GilMarSil <gilbertomarsilva@gmail.com>
2025-06-16 11:36:35 +0100

    fix: update transpilePackages config

--- apps/rh/next.config.js
+++ apps/rh/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    transpilePackages: ['@rfwebapp/ui']
-  }
+  transpilePackages: ['@rfwebapp/ui']
 };
 
 module.exports = nextConfig;
