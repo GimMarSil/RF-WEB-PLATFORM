2d0fc768772ddf0f672ab3145b22b97a6f50a8fc
GilMarSil <gilbertomarsilva@gmail.com>
2025-06-17 18:47:18 +0100

    refactor: use authority env var for msal

--- apps/core/src/authConfig.ts
+++ apps/core/src/authConfig.ts
@@ -1,6 +1,6 @@
 export const authConfig = {
   clientId: process.env.NEXT_PUBLIC_AZURE_CLIENT_ID || '',
-  tenantId: process.env.NEXT_PUBLIC_AZURE_TENANT_ID || '',
+  authority: process.env.NEXT_PUBLIC_AZURE_AUTHORITY || '',
   redirectUri:
     process.env.NEXT_PUBLIC_REDIRECT_URI || 'http://localhost:3000/core'
 };
